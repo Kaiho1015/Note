c1e9ebf1468478c1c0c3f94f351aa4be2471e851
QHP1015 <2439462872@qq.com>
2022-09-24 09:46:24 +0800

    feat:finished search

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -137,8 +137,29 @@ const config = {
           // hideable: true,
           autoCollapseCategories: true,
         }
-      }
-    }),
+      },
+      algolia: {
+        // Algolia 提供的应用 ID
+        appId: 'JVHZ8FHB9I',
+  
+        //  公开 API 密钥：提交它没有危险
+        apiKey: 'bf816047486a86b1bb3ca0419720d919',
+  
+        indexName: 'Kaiho-note',
+  
+        // 可选：见下文
+        contextualSearch: true,
+  
+        // 可选：声明哪些域名需要用 window.location 型的导航而不是 history.push。 适用于 Algolia 配置会爬取多个文档站点，而我们想要用 window.location.href 在它们之间跳转时。
+        externalUrlRegex: 'external\\.com|domain\\.com',
+  
+        // 可选：Algolia 搜索参数
+        searchParameters: {},
+  
+        // 可选：搜索页面的路径，默认启用（可以用 `false` 禁用）
+        searchPagePath: 'search',
+    },
+  })
 };
 
 module.exports = config;
